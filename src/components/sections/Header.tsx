5f53dcb209c7a0b3eae0519cc49e224cf58611a8
molinax18 <arielmolina8953@gmail.com>
2024-09-24 19:10:03 -0300

    notify cart added

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { DesktopNavbar } from './navbar/DesktopNavbar'
 import { MobileNavbar } from './navbar/MobileNavbar'
 import { CartIcon } from '../ui/icons/CartIcon'
@@ -6,12 +6,19 @@ import { LogoIcon } from '../ui/icons/LogoIcon'
 import { MenuIcon } from '../ui/icons/MenuIcon'
 import { Cart } from './Cart'
 import avatar from '../../../public/images/image-avatar.png'
+import { CartContext, useCartContext } from '../../context/CartContext'
 
 export const Header = () => {
+  const { state } = useCartContext() as CartContext
   const [showCart, setShowCart] = useState(false)
   const [showMenu, setShowMenu] = useState(false)
+  const [amountCart, setAmountCart] = useState(0)
   const toggleMenu = () => setShowMenu(!showMenu)
   const toggleCart = () => setShowCart(!showCart)
+
+  useEffect(() => {
+    setAmountCart(state.length)
+  }, [state])
 
   return (
     <header className='relative flex justify-between items-center py-3 px-4 md:py-5'>
@@ -31,14 +38,24 @@ export const Header = () => {
       </div>
       <div className='flex items-center gap-x-6'>
         <CartIcon
-          style='text-dark-grayish-blue hover-icons cursor-pointer'
-          onClick={toggleCart}
+          style='relative text-dark-grayish-blue hover-icons cursor-pointer'
+          onClick={() => {
+            toggleCart()
+            setAmountCart(0)
+          }}
         />
-        {showCart && <Cart style='absolute top-20 left-4 right-4 z-10 md:left-auto' />}
+        {amountCart > 0 && (
+          <span className='absolute bottom-8 md:bottom-10 ml-3 px-[6px] bg-orange rounded-full text-white text-xs'>
+            {state.length}
+          </span>
+        )}
+        {showCart && (
+          <Cart style='absolute top-20 left-4 right-4 z-10 md:left-auto' />
+        )}
         <img
           src={avatar}
           alt='A man with black glasses and long hair.'
-          className='size-10'
+          className='size-10 cursor-pointer rounded-full border-2 border-transparent transition hover:border-orange'
         />
       </div>
     </header>
